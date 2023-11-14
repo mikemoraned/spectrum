5f498c435c23fe659327076aedc3e1f81a02f2b1
Mike Moran <mike@houseofmoran.com>
2023-11-14 21:28:42 +0000

    center on Edinburgh

--- spikes/regions/main.js
+++ spikes/regions/main.js
@@ -1,9 +1,10 @@
 import "./style.css";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+const edinburgh = [-3.188267, 55.953251];
 const map = new mapboxgl.Map({
   container: "map", // container id
   style: "mapbox://styles/mapbox/light-v11", // stylesheet location
-  center: [-84.5, 38.05], // starting position
+  center: edinburgh, // starting position
   zoom: 12, // starting zoom
 });
