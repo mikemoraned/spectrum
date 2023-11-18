584de46fe7f3d681a920a1dfb34129d355613a0e
Mike Moran <mike@houseofmoran.com>
2023-11-18 19:19:38 +0000

    move to webapp sub-dir

--- spikes/regions/webapp/main.js
+++ spikes/regions/webapp/main.js
@@ -2,9 +2,10 @@ import "./style.css";
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 const edinburgh = [-3.188267, 55.953251];
+const style = "mapbox://styles/mapbox/streets-v12";
 const map = new mapboxgl.Map({
   container: "map", // container id
-  style: "mapbox://styles/mapbox/light-v11", // stylesheet location
+  style: style,
   center: edinburgh, // starting position
   zoom: 12, // starting zoom
 });
