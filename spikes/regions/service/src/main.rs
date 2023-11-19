9260ab9cd7f400a203a4de5d205f4d8bf49c1111
Mike Moran <mike@houseofmoran.com>
2023-11-19 16:35:50 +0000

    load GeoJSON dynamically from a service

--- spikes/regions/service/src/main.rs
+++ spikes/regions/service/src/main.rs
@@ -1,5 +1,39 @@
-use axum::{http::Method, routing::get, Router};
+use axum::{http::Method, routing::get, Json, Router};
+use geojson::{Feature, GeoJson, Geometry, Value};
 use tower_http::cors::{Any, CorsLayer};
+
+async fn layers() -> Json<GeoJson> {
+    let geometry = Geometry::new(Value::Polygon(vec![vec![
+        vec![-67.13734, 45.13745],
+        vec![-66.96466, 44.8097],
+        vec![-68.03252, 44.3252],
+        vec![-69.06, 43.98],
+        vec![-70.11617, 43.68405],
+        vec![-70.64573, 43.09008],
+        vec![-70.75102, 43.08003],
+        vec![-70.79761, 43.21973],
+        vec![-70.98176, 43.36789],
+        vec![-70.94416, 43.46633],
+        vec![-71.08482, 45.30524],
+        vec![-70.66002, 45.46022],
+        vec![-70.30495, 45.91479],
+        vec![-70.00014, 46.69317],
+        vec![-69.23708, 47.44777],
+        vec![-68.90478, 47.18479],
+        vec![-68.2343, 47.35462],
+        vec![-67.79035, 47.06624],
+        vec![-67.79141, 45.70258],
+        vec![-67.13734, 45.13745],
+    ]]));
+    let geojson = GeoJson::Feature(Feature {
+        bbox: None,
+        geometry: Some(geometry),
+        id: None,
+        properties: None,
+        foreign_members: None,
+    });
+    Json(geojson)
+}
 
 #[tokio::main]
 async fn main() {
@@ -11,6 +45,7 @@ async fn main() {
     // build our application with a single route
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
+        .route("/layers", get(layers))
         .layer(cors);
 
     // run it with hyper on localhost:3000
--- spikes/regions/webapp/main.js
+++ spikes/regions/webapp/main.js
@@ -16,53 +16,25 @@ const map = new mapboxgl.Map({
   ...starting_position,
 });
 
-async function fetchFeature() {
-  console.log("calling service ...");
-  const service_url = import.meta.env.VITE_SERVICE_BASE_URL;
+async function fetchLayers() {
+  const service_url = `${import.meta.env.VITE_SERVICE_BASE_URL}/layers`;
+  console.log("calling service ", service_url, " ...");
   const response = await fetch(service_url);
+  const geojson = response.json();
   console.log("called service");
-  const json = {
-    type: "geojson",
-    data: {
-      type: "Feature",
-      geometry: {
-        type: "Polygon",
-        // These coordinates outline Maine.
-        coordinates: [
-          [
-            [-67.13734, 45.13745],
-            [-66.96466, 44.8097],
-            [-68.03252, 44.3252],
-            [-69.06, 43.98],
-            [-70.11617, 43.68405],
-            [-70.64573, 43.09008],
-            [-70.75102, 43.08003],
-            [-70.79761, 43.21973],
-            [-70.98176, 43.36789],
-            [-70.94416, 43.46633],
-            [-71.08482, 45.30524],
-            [-70.66002, 45.46022],
-            [-70.30495, 45.91479],
-            [-70.00014, 46.69317],
-            [-69.23708, 47.44777],
-            [-68.90478, 47.18479],
-            [-68.2343, 47.35462],
-            [-67.79035, 47.06624],
-            [-67.79141, 45.70258],
-            [-67.13734, 45.13745],
-          ],
-        ],
-      },
-    },
-  };
-  return json;
+  return geojson;
 }
 
 map.on("load", () => {
   console.log("loading ...");
 
-  fetchFeature().then((json) => {
-    map.addSource("maine", json);
+  fetchLayers().then((geojson) => {
+    const source = {
+      type: "geojson",
+      data: geojson,
+    };
+
+    map.addSource("maine", source);
 
     map.addLayer({
       id: "maine",
