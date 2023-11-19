3ab1f8718e1b3fe0f7da273db4942488ea7431ee
Mike Moran <mike@houseofmoran.com>
2023-11-19 18:27:34 +0000

    writing to geojson but not showing up as correct place

--- spikes/regions/service/src/bin/meadows.rs
+++ spikes/regions/service/src/bin/meadows.rs
@@ -1,7 +1,12 @@
+use geojson::{Feature, GeoJson, Geometry, Position, Value};
 use osmpbf::{Element, IndexedReader};
+use std::fs::OpenOptions;
+use std::io::Write;
 
 fn main() -> Result<(), ()> {
     let mut reader = IndexedReader::from_path("data/edinburgh_scotland.osm.pbf").unwrap();
+
+    let mut positions: Vec<Position> = vec![];
 
     reader
         .read_ways_and_deps(
@@ -24,12 +29,34 @@ fn main() -> Result<(), ()> {
                             dense_node.lat(),
                             dense_node.lon()
                         );
+                        let position = vec![dense_node.lon(), dense_node.lat()];
+                        positions.push(position);
                     }
                     _ => (),
                 }
             },
         )
         .unwrap();
+
+    let geometry = Geometry::new(Value::Polygon(vec![positions]));
+
+    let geojson = GeoJson::Feature(Feature {
+        bbox: None,
+        geometry: Some(geometry),
+        id: None,
+        properties: None,
+        foreign_members: None,
+    });
+
+    let geojson_string = geojson.to_string();
+
+    let mut f = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("data/meadows.json")
+        .unwrap();
+    f.write_all(geojson_string.as_bytes()).unwrap();
 
     Ok(())
 }
