ae964d09dcd4b4c17957e7ddbffe35621822694f
Mike Moran <mike@houseofmoran.com>
2023-11-19 17:07:58 +0000

    count relations

--- spikes/regions/service/src/bin/pbfinfo.rs
+++ spikes/regions/service/src/bin/pbfinfo.rs
@@ -2,18 +2,18 @@ use osmpbf::{Element, ElementReader};
 
 fn main() -> Result<(), ()> {
     let reader = ElementReader::from_path("data/edinburgh_scotland.osm.pbf").unwrap();
-    let mut ways = 0_u64;
+    let mut relations = 0_u64;
 
     // Increment the counter by one for each way.
     reader
         .for_each(|element| {
-            if let Element::Way(_) = element {
-                ways += 1;
+            if let Element::Relation(_) = element {
+                relations += 1;
             }
         })
         .unwrap();
 
-    println!("Number of ways: {ways}");
+    println!("Number of relations: {relations}");
 
     Ok(())
 }
