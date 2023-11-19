c2c90bca131a31a14be34b7909d3f91c20c24cbb
Mike Moran <mike@houseofmoran.com>
2023-11-19 18:03:15 +0000

    use `IndexedReader` to find parts of meadows

--- spikes/regions/service/src/bin/meadows.rs
+++ spikes/regions/service/src/bin/meadows.rs
@@ -1,19 +1,34 @@
-use osmpbf::{Element, ElementReader};
+use osmpbf::{Element, IndexedReader};
 
 fn main() -> Result<(), ()> {
-    let reader = ElementReader::from_path("data/edinburgh_scotland.osm.pbf").unwrap();
+    let mut reader = IndexedReader::from_path("data/edinburgh_scotland.osm.pbf").unwrap();
 
     reader
-        .for_each(|element| {
-            if let Element::Way(way) = element {
-                if way
-                    .tags()
-                    .any(|(name, value)| name == "name" && value == "The Meadows")
-                {
-                    println!("Found meadows: {}", way.id());
+        .read_ways_and_deps(
+            |way| {
+                way.tags()
+                    .any(|key_value| key_value == ("name", "The Meadows"))
+            },
+            |element| {
+                // Increment counter
+                match element {
+                    Element::Way(way) => {
+                        println!("way: {:?}", way.id());
+                    }
+                    Element::Node(node) => {
+                        println!("node: lat: {}, lon: {}", node.lat(), node.lon());
+                    }
+                    Element::DenseNode(dense_node) => {
+                        println!(
+                            "dense_node: lat: {}, lon: {}",
+                            dense_node.lat(),
+                            dense_node.lon()
+                        );
+                    }
+                    _ => (),
                 }
-            }
-        })
+            },
+        )
         .unwrap();
 
     Ok(())
