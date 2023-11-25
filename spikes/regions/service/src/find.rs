6507fb75c9bbf0fb82a06ec3701dbf1f7a3fe23c
Mike Moran <mike@houseofmoran.com>
2023-11-25 18:01:55 +0000

    extract `insert_position_into_way`

--- spikes/regions/service/src/find.rs
+++ spikes/regions/service/src/find.rs
@@ -7,29 +7,27 @@ pub fn find() -> Result<GeoJson, ()> {
     let mut reader = IndexedReader::from_path("data/edinburgh_scotland.osm.pbf").unwrap();
 
     fn way_filter(way: &osmpbf::Way<'_>) -> bool {
-        way.tags().any(|key_value| key_value == ("leisure", "park"))
+        let pairs = vec![
+            ("leisure", "park"),
+            // ("leisure", "garden")
+        ];
+        way.tags().any(|key_value| pairs.contains(&key_value))
     }
 
-    let mut pending_refs: HashMap<i64, _> = HashMap::new();
+    let mut pending_refs = HashMap::new();
 
     reader
-        .read_ways_and_deps(way_filter, |element| {
-            // Increment counter
-            match element {
-                Element::Way(way) => {
-                    println!("way: {:?}", way.id());
-                    let mut pending_refs_for_way = vec![];
-                    way.refs().for_each(|r| {
-                        pending_refs_for_way.push(r);
-                    });
-                    pending_refs.insert(way.id(), pending_refs_for_way);
-                }
-                _ => (),
+        .read_ways_and_deps(way_filter, |element| match element {
+            Element::Way(way) => {
+                let mut pending_refs_for_way = vec![];
+                way.refs().for_each(|r| {
+                    pending_refs_for_way.push(r);
+                });
+                pending_refs.insert(way.id(), pending_refs_for_way);
             }
+            _ => (),
         })
         .unwrap();
-
-    println!("pending refs: {:?}", pending_refs);
 
     let mut positions_for_way: HashMap<i64, Vec<Position>> = HashMap::new();
     for (way_id, pending_refs) in pending_refs.iter() {
@@ -38,47 +36,41 @@ pub fn find() -> Result<GeoJson, ()> {
         positions_for_way.insert(*way_id, positions);
     }
 
+    fn insert_position_into_way(
+        node_id: i64,
+        position: &Position,
+        pending_refs: &HashMap<i64, Vec<i64>>,
+        positions_for_way: &mut HashMap<i64, Vec<Vec<f64>>>,
+    ) {
+        for (way_id, pending_refs) in pending_refs.iter() {
+            let positions = positions_for_way.get_mut(way_id).unwrap();
+            for i in 0..pending_refs.len() {
+                if pending_refs[i] == node_id {
+                    positions[i] = position.clone();
+                }
+            }
+        }
+    }
+
     reader
         .read_ways_and_deps(way_filter, |element| match element {
             Element::DenseNode(dense_node) => {
-                println!(
-                    "dense_node: lat: {}, lon: {}",
-                    dense_node.lat(),
-                    dense_node.lon()
-                );
                 let position = vec![dense_node.lon(), dense_node.lat()];
-                for (way_id, pending_refs) in pending_refs.iter() {
-                    let positions = positions_for_way.get_mut(way_id).unwrap();
-                    for i in 0..pending_refs.len() {
-                        if pending_refs[i] == dense_node.id() {
-                            positions[i] = position.clone();
-                            println!(
-                                "slotting in dense_node {} at position {} in way {}",
-                                dense_node.id(),
-                                i,
-                                way_id
-                            );
-                        }
-                    }
-                }
+                insert_position_into_way(
+                    dense_node.id(),
+                    &position,
+                    &pending_refs,
+                    &mut positions_for_way,
+                );
             }
             Element::Node(node) => {
-                println!("node: lat: {}, lon: {}", node.lat(), node.lon());
                 let position = vec![node.lon(), node.lat()];
-                for (way_id, pending_refs) in pending_refs.iter() {
-                    let positions = positions_for_way.get_mut(way_id).unwrap();
-                    for i in 0..pending_refs.len() {
-                        if pending_refs[i] == node.id() {
-                            positions[i] = position.clone();
-                            println!(
-                                "slotting in node {} at position {} in way {}",
-                                node.id(),
-                                i,
-                                way_id
-                            );
-                        }
-                    }
-                }
+                insert_position_into_way(
+                    node.id(),
+                    &position,
+                    &pending_refs,
+                    &mut positions_for_way,
+                );
             }
             _ => (),
         })
