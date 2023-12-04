436acc1a4a8815e4ee02b2da6fb6e3ea63896fcb
Mike Moran <mike@houseofmoran.com>
2023-12-04 16:26:09 +0000

    extend to wasm test

--- spikes/regions-wasm/counter/src/lib.rs
+++ spikes/regions-wasm/counter/src/lib.rs
@@ -1,8 +1,12 @@
+use wasm_bindgen::prelude::wasm_bindgen;
+
 #[derive(PartialEq, Debug)]
+#[wasm_bindgen]
 pub struct Counter {
     count: u8,
 }
 
+#[wasm_bindgen]
 impl Counter {
     pub fn new() -> Counter {
         Counter { count: 0 }
@@ -11,9 +15,12 @@ impl Counter {
 
 #[cfg(test)]
 mod tests {
+    use wasm_bindgen_test::wasm_bindgen_test;
+
     use super::*;
 
     #[test]
+    #[wasm_bindgen_test]
     fn new() {
         let result = Counter::new();
         assert_eq!(result, Counter { count: 0 });
