889e5f69cd9808e23d4c492481da5c2b605ea9c5
Mike Moran <mike@houseofmoran.com>
2023-12-04 16:30:01 +0000

    add `get_count`

--- spikes/regions-wasm/counter/src/lib.rs
+++ spikes/regions-wasm/counter/src/lib.rs
@@ -15,6 +15,10 @@ impl Counter {
     pub fn set_count(&mut self, value: u8) {
         self.count = value;
     }
+
+    pub fn get_count(&self) -> u8 {
+        self.count
+    }
 }
 
 #[cfg(test)]
@@ -36,5 +40,14 @@ mod tests {
         let mut counter = Counter::new();
         counter.set_count(12);
         assert_eq!(counter, Counter { count: 12 });
+    }
+
+    #[test]
+    #[wasm_bindgen_test]
+    fn get_count() {
+        let mut counter = Counter::new();
+        assert_eq!(counter.get_count(), 0);
+        counter.set_count(12);
+        assert_eq!(counter.get_count(), 12);
     }
 }
