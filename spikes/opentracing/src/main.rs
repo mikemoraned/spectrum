9c8c9bb2863c5a21fd7a8c90257abfc002604b2d
Mike Moran <mike@houseofmoran.com>
2023-12-24 17:53:30 +0100

    basic "Hello, World!"

--- spikes/opentracing/src/main.rs
+++ spikes/opentracing/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use axum::{routing::get, Router};
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:10000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
