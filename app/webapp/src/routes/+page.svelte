7e8218759e508c9e7b2d142081acc588b7fc0b7e
Mike Moran <mike@houseofmoran.com>
2024-08-01 13:57:33 +0200

    add basic mapbox setup

--- app/webapp/src/routes/+page.svelte
+++ app/webapp/src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// @ts-nocheck
+
+	import mapbox from 'mapbox-gl';
+	const { Map } = mapbox;
+	import { onMount, onDestroy } from 'svelte';
+
+	import { PUBLIC_MAPBOX_TOKEN } from '$env/static/public';
+
+	let map;
+	let mapContainer;
+	let lng, lat, zoom;
+
+	lng = -71.224518;
+	lat = 42.213995;
+	zoom = 9;
+
+	onMount(() => {
+		const initialState = { lng: lng, lat: lat, zoom: zoom };
+
+		map = new Map({
+			container: mapContainer,
+			accessToken: PUBLIC_MAPBOX_TOKEN,
+			style: `mapbox://styles/mapbox/outdoors-v11`,
+			center: [initialState.lng, initialState.lat],
+			zoom: initialState.zoom
+		});
+	});
+
+	onDestroy(() => {
+		if (map) {
+			map.remove();
+		}
+	});
+</script>
+
+<div class="map-wrap">
+	<div class="map" bind:this={mapContainer} />
+</div>
+
+<style>
+	.map {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+</style>
