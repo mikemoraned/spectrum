d0470154de7d8acab49c05f6703356d35118bfb1
Mike Moran <mike@houseofmoran.com>
2024-08-01 20:16:00 +0200

    add tracing setup, sending traces to honeycomb, working locally

--- app/service/api/src/main.rs
+++ app/service/api/src/main.rs
@@ -1,11 +1,48 @@
-use axum::{routing::get, Router};
+use api::tracing::{init_opentelemetry_from_environment, init_safe_default_from_environment};
+use axum::{http::StatusCode, routing::get, Router};
+use clap::Parser;
+use tracing::info;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// enable opentelemetry
+    #[arg(long)]
+    opentelemetry: bool,
+}
+
+#[tracing::instrument()]
+async fn health() -> StatusCode {
+    StatusCode::NO_CONTENT
+}
 
 #[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt::init();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    if args.opentelemetry {
+        match init_opentelemetry_from_environment("spectrum") {
+            Ok(_) => {
+                info!("Opentelemetry initialised")
+            }
+            Err(e) => {
+                println!(
+                    "Failed to initialise Opentelemetry ('{:?}'), falling back to default",
+                    e
+                );
+                init_safe_default_from_environment()?;
+            }
+        }
+    } else {
+        init_safe_default_from_environment()?;
+    }
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(health));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
